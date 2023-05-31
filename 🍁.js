b9ba29e7122c694bd9f78868dc9d37d1b6637fc3
IRullDev <134046625+IRullDev@users.noreply.github.com>
2023-05-31 13:48:14 +0700

    Add files via upload

---
+++ 🍁.js
@@ -0,0 +1,200 @@
+[
+  {
+    index: 0,
+    autor: 'Akio',
+    fgwork: 'Kamu bekerja sebagai pemotong kue dan menghasilkan'
+  },
+  {
+    index: 1,
+    autor: 'Akio',
+    fgwork: 'Dia bekerja untuk sebuah perusahaan militer swasta, berpenghasilan'
+  },
+  {
+    index: 2,
+    autor: 'Akio',
+    fgwork: 'Atur acara mencicipi anggur dan dapatkan'
+  },
+  {
+    index: 3,
+    autor: 'Akio',
+    fgwork: 'Kamu diculik dan dibawa ke coliseum bawah tanah tempat Kamu melawan monster dengan orang yang belum pernah Kamu temui sebelumnya. Menginginkan'
+  },
+  {
+    index: 4,
+    autor: 'Akio',
+    fgwork: 'Kamu membersihkan cerobong asap dan menemukan'
+  },
+  {
+    index: 5,
+    autor: 'Akio',
+    fgwork: 'Kembangkan game untuk mencari nafkah dan menangkan kemenangan'
+  },
+  {
+    index: 6,
+    autor: 'Akio',
+    fgwork: 'Mengapa perintah ini disebut bekerja? Kamu bahkan tidak melakukan apa pun yang berhubungan dengan pekerjaan. bagaimanapun Kamu menang'
+  },
+  {
+    index: 7,
+    autor: 'Akio',
+    fgwork: 'Kamu bekerja di kantor lembur untuk'
+  },
+  {
+    index: 8,
+    autor: 'Akio',
+    fgwork: 'Kamu bekerja sebagai penculik pengantin dan menghasilkan uang'
+  },
+  {
+    index: 9,
+    autor: 'Akio',
+    fgwork: 'Seseorang datang dan bermain. untuk melihat mereka memberi Kamu'
+  },
+  {
+    index: 10,
+    autor: 'Akio',
+    fgwork: 'Kamu membeli dan menjual item dan Menghasilkan'
+  },
+  {
+    index: 11,
+    autor: 'Akio',
+    fgwork: 'Kamu bekerja di restoran Nenek sebagai juru masak dan mendapat penghasilan'
+  },
+  {
+    index: 12,
+    autor: 'Akio',
+    fgwork: 'Trabajas 10 minutos en un Pizza Hut local. Ganaste'
+  },
+  {
+    index: 13,
+    autor: 'Akio',
+    fgwork: 'Kamu bekerja sebagai penulis kue keberuntungan dan menghasilkan'
+  },
+  {
+    index: 14,
+    autor: 'Akio',
+    fgwork: 'Kamu memeriksa tas Kamu dan memutuskan untuk menjual beberapa barang tidak berguna yang tidak Kamu butuhkan. Ternyata semua omong kosong itu berharga'
+  },
+  {
+    index: 15,
+    autor: 'Akio',
+    fgwork: 'Kamu melihat seseorang berjuang untuk memasukkan kotak ke dalam mobilnya, Kamu bergegas membantunya sebelum mereka terluka. Setelah membantu mereka, mereka dengan baik hati memberi Kamu '
+  },
+  {
+    index: 16,
+    autor: 'Akio',
+    fgwork: 'Kamu mengembangkan game untuk mencari nafkah dan Kamu menang '
+  },
+  {
+    index: 17,
+    autor: 'Akio',
+    fgwork: 'Trabajas todo el día en la empresa por'
+  },
+  {
+    index: 18,
+    autor: 'Akio',
+    fgwork: 'Ayudas a moderar el grupo de DyLux por'
+  },
+  {
+    index: 19,
+    autor: 'Akio',
+    fgwork: 'Kamu mendesain logo untuk *google* dan mendapat'
+  },
+  {
+    index: 20,
+    autor: 'Akio',
+    fgwork: 'Kamu mengambil alih *Google*, kamu mendapat'
+  },
+  {
+    index: 21,
+    autor: 'Akio',
+    fgwork: 'Dia bekerja dengan kemampuan terbaiknya di sebuah perusahaan percetakan yang sedang merekrut dan mendapatkan bagiannya yang adil!'
+  },
+  {
+    index: 22,
+    autor: 'Akio',
+    fgwork: 'Kamu bekerja sebagai pemangkas semak untuk *Akio* dan menghasilkan'
+  },
+  {
+    index: 23,
+    autor: 'Akio',
+    fgwork: 'Permintaan untuk game seluler meningkat, jadi Kamu membuat game baru yang penuh dengan transaksi mikro. Dengan game baru Kamu, Kamu menghasilkan total'
+  },
+  {
+    index: 24,
+    autor: 'Akio',
+    fgwork: 'Kamu bekerja sebagai pengisi suara untuk Spongebob dan Kamu berhasil menang'
+  },
+  { index: 25, autor: 'Akio', fgwork: 'Estabas cultivando y Ganaste' },
+  {
+    index: 26,
+    autor: 'Akio',
+    fgwork: 'Kamu bekerja sebagai pembuat istana pasir dan menghasilkan'
+  },
+  {
+    index: 27,
+    autor: 'Akio',
+    fgwork: 'Kamu bekerja sebagai seniman jalanan dan menghasilkan'
+  },
+  {
+    index: 28,
+    autor: 'Akio',
+    fgwork: 'Kamu melakukan pekerjaan sosial untuk tujuan yang baik! untuk tujuan baik Kamu yang Kamu terima'
+  },
+  {
+    index: 29,
+    autor: 'Akio',
+    fgwork: 'Memperbaiki tank T-60 yang cacat di Afghanistan. Para kru membayar Kamu'
+  },
+  {
+    index: 30,
+    autor: 'Akio',
+    fgwork: 'Kamu bekerja sebagai ahli ekologi belut dan menghasilkan'
+  },
+  {
+    index: 31,
+    autor: 'Akio',
+    fgwork: 'Kamu bekerja di Disneyland sebagai pKamu berkostum dan Kamu mendapat penghasilan'
+  },
+  {
+    index: 32,
+    autor: 'Akio',
+    fgwork: 'Kamu memperbaiki mesin arcade dan menerima'
+  },
+  {
+    index: 33,
+    autor: 'Akio',
+    fgwork: 'Kamu melakukan beberapa pekerjaan sambilan di sekitar kota dan Kamu menang'
+  },
+  {
+    index: 34,
+    autor: 'Akio',
+    fgwork: 'Kamu membersihkan beberapa cetakan beracun dari ventilasi dan menang'
+  },
+  {
+    index: 35,
+    autor: 'Akio',
+    fgwork: 'Kamu memecahkan misteri wabah kolera dan pemerintah menghadiahi Kamu sejumlah uang'
+  },
+  { index: 36, autor: 'Akio', fgwork: 'Kamu bekerja sebagai ahli zoologi dan menghasilkan' },
+  { index: 37, autor: 'Akio', fgwork: 'Kamu bekerja dan Kamu menghasilkan' },
+  {
+    index: 38,
+    autor: 'Akio',
+    fgwork: 'Kamu menjual sandwich ikan dan mendapatkannya'
+  },
+  {
+    index: 39,
+    autor: 'Akio',
+    fgwork: 'Kamu memperbaiki mesin arcade dan menerima'
+  },
+  {
+    index: 40,
+    autor: 'Akio',
+    fgwork: 'Hanya karena berada di grup *DyLux - FG* Kamu menang!'
+  },
+  {
+    index: 41,
+    autor: 'Akio',
+    fgwork: 'Kamu melakukan beberapa pekerjaan sosial untuk tujuan yang baik! Apakah Kamu menerima!'
+  }
+]
